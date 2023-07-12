8e9329f2e3cbe9a4fdcd5813117e9538833ea381
Aiswaryaramesh99 <aiswaryalaxmiramesh@gmail.com>
2023-07-12 10:25:16 +0530

    Initial commit

---
+++ app/src/main/java/id/codetr/bgclrr/MainActivity.java
@@ -0,0 +1,29 @@
+package id.codetr.bgclrr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private int[] mColorChoices =
+            { Color.BLACK, Color.BLUE, Color.CYAN,
+                    Color.DKGRAY, Color.GRAY, Color.GREEN,
+                    Color.LTGRAY, Color.MAGENTA, Color.RED,
+                    Color.WHITE, Color.YELLOW };
+    private TextView mTextRegion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void randomizeColor(View button) {
+        Random generator = new Random();
+        int index = generator.nextInt(mColorChoices.length);
+        mTextRegion.setBackgroundColor(mColorChoices[index]);
+    }
+}
